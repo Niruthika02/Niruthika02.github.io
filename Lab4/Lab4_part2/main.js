3e30910e998491187f3adfb7afebb55ce208d0a8
Niruthika02 <niruthika.sivaruban@dcmail.ca>
2023-11-26 03:53:41 -0500

    made all the relevant final changes for assignment 04 part 2
    made all the relevant final changes for assignment 04 part 2

--- Lab4/Lab4_part2/main.js
+++ Lab4/Lab4_part2/main.js
@@ -1,37 +1,40 @@
+// Get references to HTML elements
 const displayedImage = document.querySelector('.displayed-img');
 const thumbBar = document.querySelector('.thumb-bar');
-
 const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
-/* Declaring the array of image filenames */
+// Declare an array of image filenames
+const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 
-const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+// Define alternative text for each image
 const alts = {
   'pic1.jpg' : 'Closeup of a human eye',
   'pic2.jpg' : 'Rock that looks like a wave',
   'pic3.jpg' : 'Purple and white pansies',
-  'pic4.jpg' : 'Section of wall from a pharoah\'s tomb',
+  'pic4.jpg' : 'Section of wall from a pharaoh\'s tomb',
   'pic5.jpg' : 'Large moth on a leaf'
 }
 
-/* Looping through images */
-
+// Loop through the images array to create thumbnail images in the thumb-bar
 for (const image of images) {
   const newImage = document.createElement('img');
   newImage.setAttribute('src', `images/${image}`);
   newImage.setAttribute('alt', alts[image]);
   thumbBar.appendChild(newImage);
+
+  // Add a click event listener to each thumbnail image
   newImage.addEventListener('click', e => {
     displayedImage.src = e.target.src;
     displayedImage.alt = e.target.alt;
   });
 }
 
-/* Wiring up the Darken/Lighten button */
-
+// Add a click event listener to the Darken/Lighten button
 btn.addEventListener('click', () => {
   const btnClass = btn.getAttribute('class');
+
+  // Toggle between Darken and Lighten states
   if (btnClass === 'dark') {
     btn.setAttribute('class','light');
     btn.textContent = 'Lighten';
